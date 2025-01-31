a7311cdb8d8c7767eb8040cbbb9e8c8a7a052dbc
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-01-31 23:09:41 +0600

    basic

---
+++ basic dart.dart
@@ -0,0 +1,31 @@
+void main() { 
+   print('hello tanvir'); 
+   var name="tanvir";
+   var roll=123;
+   print("my name is ${name} and my roll no is ${roll}");
+
+   int marks=80;
+   double pi= 3.1416;
+   var msg="hello tanvir bhai";
+
+   bool isValid=true;
+    var list=[1,2,3,4,5];
+
+    // dart map
+
+    // var student={
+    //     'name':'tanvir',
+    //     'roll':123,
+    //     'marks':80,
+    //     'age':99,
+    //     'dept':cse'}
+
+    var student={ 'name': 'tanvir', 'id': 5307, 'age':87};
+
+    print(student['name']);
+
+
+            
+
+
+}
