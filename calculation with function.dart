04b4667fd8e5be4c7c2557c27554c9163973c68d
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-02-01 20:11:46 +0600

    simnple function

---
+++ calculation with function.dart
@@ -0,0 +1,12 @@
+void calculalateInterest(double principal, double rate, double time){
+  double interest= principal* rate*time/100;
+  print("simple interest is $interest");
+}
+
+void main(){
+  double principal=5000;
+  double time=4;
+  double rate=3;
+
+  calculalateInterest(principal, rate, time);
+}
