487b42f75e3edd4c952b833a4939547af5382265
Tanvir Ahmed <110832885+tanvir-talha058@users.noreply.github.com>
2025-02-07 23:32:44 +0600

    highest second number

---
+++ lab report 1.1.dart
@@ -0,0 +1,18 @@
+int Second_Highest_value(List<int> numbers) {
+  if (numbers.length < 2) {
+    throw Exception("List must contain at least two elements.");
+  }
+  numbers = numbers.toSet().toList(); 
+  numbers.sort(); 
+  return numbers[numbers.length - 2];
+}
+
+void main() {
+  List<int> numbers = [10, 20, 5, 40, 30, 40];
+  print("Second Highest Value: ${Second_Highest_value(numbers)}");
+}
+
+
+
+
+
