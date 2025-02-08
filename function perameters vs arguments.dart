e6da78e4ad298bb1efa2f11487fc86e713a4cbf9
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-02-08 19:24:07 +0600

    function

--- function perameters vs arguments.dart
+++ function perameters vs arguments.dart
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-void add(int num1, int num2){
-  int sum;
-  sum=num1+num2;
-
+void add(int num1, int num2) {
+  int sum = num1 + num2;
   print("The sum is $sum");
 }
 
-void main(){
-  add(10,20);
-=======
-void add(int num1, int num2){
-  int sum;
-  sum=num1+num2;
-
-  print("The sum is $sum");
-}
-
-void main(){
-  add(10,20);
->>>>>>> b17e8f04922cc46143a322a15f5c5a9b3e74fb3c
+void main() {
+  add(10, 20);
 }
--- function with parameter and no return types.dart
+++ function with parameter and no return types.dart
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-void add(int a, int b){
-  int sum=a+b;
-  print("the sum is $sum");
+void add(int a, int b) {
+  int sum = a + b;
+  print("The sum is $sum");
 }
 
-void main(){
-  int num1=13;
-  int num2=58; 
+void main() {
+  int num1 = 13;
+  int num2 = 58; 
   
-  add( num1, num2);
-  
-
-=======
-void add(int a, int b){
-  int sum=a+b;
-  print("the sum is $sum");
-}
-
-void main(){
-  int num1=13;
-  int num2=58; 
-  
-  add( num1, num2);
-  
-
->>>>>>> b17e8f04922cc46143a322a15f5c5a9b3e74fb3c
+  add(num1, num2);
 }
