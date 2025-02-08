7a144f937145e09de104c098307c9a1a1d4a9771
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-02-08 23:37:25 +0600

    default value

---
+++ default value on positional parameter.dart
@@ -0,0 +1,10 @@
+void printInfo(String name, String gender, [String title="sir/ma'am"]){
+  print("hello $title $name your gender is $gender");
+
+}
+
+void main (){
+  printInfo("tanvir","male");
+  printInfo("Tanvir","male","Mr.");
+  printInfo("sumu","female","Ms.");
+}
