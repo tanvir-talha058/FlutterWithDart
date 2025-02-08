746632d68f1d19c934ebc9eeb0af80c7f1ee8476
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-02-08 22:48:39 +0600

    function

---
+++ complete_function.dart
@@ -0,0 +1,38 @@
+int add (int a, int b){
+  var total=a+b;
+  return total;
+
+}
+
+//parameter and no return type
+
+void mul(int a, int b){
+  var total;
+  total=a*b;
+  print("Mutiplication is:$total");
+}
+
+//no parameter and return type
+
+String greet(){
+  String greet="Welcome";
+  return greet;
+}
+
+//no paremeter and no return type
+
+void greetings(){
+  print("hello tanviir");
+}
+
+
+
+void main(){
+  var total=add(3,4);
+  print("Total sum:$total");
+  mul(2,3);
+  var greeting =greet();
+  print("Greeting: $greeting");
+
+  greetings();
+}
