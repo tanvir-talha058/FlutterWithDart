16486051633300412956986d577bc55419304caa
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-02-08 19:15:12 +0600

    lab 1

--- lab report 1.1.dart
+++ lab report 1.1.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 int Second_Highest_value(List<int> numbers) {
   if (numbers.length < 2) {
     throw Exception("List must contain at least two elements.");
@@ -12,28 +11,3 @@ void main() {
   List<int> numbers = [10, 20, 5, 40, 30, 40];
   print("Second Highest Value: ${Second_Highest_value(numbers)}");
 }
-
-
-
-
-
-=======
-int Second_Highest_value(List<int> numbers) {
-  if (numbers.length < 2) {
-    throw Exception("List must contain at least two elements.");
-  }
-  numbers = numbers.toSet().toList(); 
-  numbers.sort(); 
-  return numbers[numbers.length - 2];
-}
-
-void main() {
-  List<int> numbers = [10, 20, 5, 40, 30, 40];
-  print("Second Highest Value: ${Second_Highest_value(numbers)}");
-}
-
-
-
-
-
->>>>>>> b17e8f04922cc46143a322a15f5c5a9b3e74fb3c
