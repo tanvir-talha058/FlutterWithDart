5a0477351c8d9accad61291b7815f5c2a643e599
tanvir-talha058 <tanvirahmed123000@gmail.com>
2025-02-14 20:55:18 +0600

    practiced the .isvalue

---
+++ practice.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  int value1=11;
+  int value2=16;
+
+  print(value1.isOdd);
+  print(value2.isEven);
+  
+  print(value1.isFinite);
+  print(value2.isInfinite);
+  print(value2.isEven);
+}
